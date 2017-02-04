3197fd6e01e701f7ee204cf2a8c23fa8235efdd4
HerrSubset <smets.pj@gmail.com>
2017-02-04 17:18:15 +0100

    add link to details page to MinimalFederationDao

--- src/main/java/com/clubtools/belgianvolleycompetitionapi/core/dao/MinimalFederationDao.java
+++ src/main/java/com/clubtools/belgianvolleycompetitionapi/core/dao/MinimalFederationDao.java
@@ -1,10 +1,16 @@
 package com.clubtools.belgianvolleycompetitionapi.core.dao;
 
+import com.clubtools.belgianvolleycompetitionapi.rest.FederationController;
+import org.springframework.hateoas.ResourceSupport;
+
+import static org.springframework.hateoas.core.DummyInvocationUtils.methodOn;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+
 /**
  * @Author: HerrSubset
  * @Created: 2/4/17
  */
-public class MinimalFederationDao {
+public class MinimalFederationDao extends ResourceSupport {
 
     public final String name;
     public final String abbreviation;
@@ -12,5 +18,10 @@ public class MinimalFederationDao {
     public MinimalFederationDao(String name, String abbreviation) {
         this.name = name;
         this.abbreviation = abbreviation;
+
+        // links for further querying
+        this.add(linkTo(methodOn(FederationController.class)
+                .getFederation(abbreviation.toLowerCase()))
+                .withRel("details"));
     }
 }
--- src/main/java/com/clubtools/belgianvolleycompetitionapi/rest/FederationController.java
+++ src/main/java/com/clubtools/belgianvolleycompetitionapi/rest/FederationController.java
@@ -1,8 +1,13 @@
 package com.clubtools.belgianvolleycompetitionapi.rest;
 
+import com.clubtools.belgianvolleycompetitionapi.core.dao.FederationDao;
 import com.clubtools.belgianvolleycompetitionapi.core.dao.TotalOverviewDao;
 import com.clubtools.belgianvolleycompetitionapi.core.service.FederationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +22,13 @@ public class FederationController {
     private FederationService service;
 
     @RequestMapping("/federations")
-    public TotalOverviewDao getOverview() {
-        return service.getOverview();
+    public HttpEntity<TotalOverviewDao> getOverview() {
+        TotalOverviewDao overview = service.getOverview();
+        return new ResponseEntity<>(overview, HttpStatus.OK);
+    }
+
+    @RequestMapping("/federations/{abbreviation}")
+    public HttpEntity<FederationDao> getFederation(@PathVariable String abbreviation) {
+        return null;
     }
 }
