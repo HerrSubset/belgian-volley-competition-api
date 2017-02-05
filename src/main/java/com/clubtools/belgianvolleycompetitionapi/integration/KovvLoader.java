8febe2b6934112e1caf3bad9ac66ebda302fc03f
HerrSubset <smets.pj@gmail.com>
2017-02-05 01:30:08 +0100

    load KOVV game data dynamically

--- src/main/java/com/clubtools/belgianvolleycompetitionapi/integration/KovvLoader.java
+++ src/main/java/com/clubtools/belgianvolleycompetitionapi/integration/KovvLoader.java
@@ -1,5 +1,6 @@
 package com.clubtools.belgianvolleycompetitionapi.integration;
 
+import com.clubtools.belgianvolleycompetitionapi.domain.Game;
 import com.clubtools.belgianvolleycompetitionapi.domain.League;
 import com.clubtools.belgianvolleycompetitionapi.domain.LeagueId;
 import com.google.common.io.CharStreams;
@@ -12,6 +13,7 @@ import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.HttpClients;
@@ -22,7 +24,10 @@ import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
@@ -36,6 +41,7 @@ public class KovvLoader implements FederationLoader {
     private final static String API_PATH = "/AjaxVVBWedstijden.php";
     private final static String ACTION = "wattedoen";
     private final static String ACTION_FIND_LEAGUES = "2";
+    private final static String ACTION_FIND_GAMES = "1";
     private final static String REEKS_TYPE = "reekstype";
     private final static String PROVINCIE = "provincie";
     private final static String REEKSNAAM = "reeksnaam";
@@ -98,11 +104,81 @@ public class KovvLoader implements FederationLoader {
 
     @Override
     public League getLeague(LeagueId leagueId) {
-        return null;
+        JsonArray jsonArray = getLeagueGamesJsonArray(leagueId.getId());
+        Iterator<JsonElement> iterator = jsonArray.iterator();
+        List<Game> games = new ArrayList<>();
+
+        while (iterator.hasNext()) {
+            JsonObject object = iterator.next().getAsJsonObject();
+            games.add(createGame(object));
+        }
+
+        return new League(leagueId, games);
     }
 
     @Override
     public LeagueId getLeagueId(String leagueSlug) {
-        return null;
+        for (LeagueId id : getLeagueIds()) {
+            if (id.getId().equalsIgnoreCase(leagueSlug)) return id;
+        }
+        throw new RuntimeException("could not find league with id " + leagueSlug);
+
+    }
+
+    private Game createGame(JsonObject object) {
+        String homeTeam = object.get("thuisploeg").getAsString().trim();
+        String awayTeam = object.get("bezoekers").getAsString().trim();
+        String day = object.get("Datum").getAsString().trim();
+        String time = object.get("aanvangsuur").getAsString().trim();
+
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+        Date date;
+        try {
+            String fullDateString = day + " " + time;
+            date = format.parse(fullDateString);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+        return new Game(homeTeam, awayTeam, date);
+    }
+
+    private JsonArray getLeagueGamesJsonArray(String leagueId) {
+
+        URI uri;
+        try {
+            uri = new URIBuilder().setScheme("http")
+                    .setHost(API_HOST)
+                    .setPath(API_PATH)
+                    .setParameter(ACTION, ACTION_FIND_GAMES)
+                    .setParameter("provincie", "6")
+                    .setParameter("reeks", leagueId.toUpperCase())
+                    .setParameter("competitie", "1")
+                    .setParameter("team", "0")
+                    .setParameter("datumbegin", "0")
+                    .setParameter("datumeind", "0")
+                    .setParameter("stamnummer", "0")
+                    .build();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        HttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(uri);
+
+
+        // perform request
+        String json;
+        try {
+            HttpResponse response = httpClient.execute(httpGet);
+            HttpEntity entity = response.getEntity();
+            json = CharStreams.toString(new InputStreamReader(entity.getContent()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        JsonParser parser = new JsonParser();
+        JsonElement element = parser.parse(json);
+        return element.getAsJsonArray();
     }
 }
